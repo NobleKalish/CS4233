2e61c6cdb863f2708b8ab294c5115f96872dfd7a
Noble Kalish <noblekalish1220@gmail.com>
2020-04-26 20:07:08 -0600

    beta init

---
+++ Escape-nkalish/src/escape/SquareGameManager.java
@@ -0,0 +1,26 @@
+package escape;
+
+import escape.board.coordinate.SquareCoordinate;
+import escape.piece.EscapePiece;
+
+public class SquareGameManager implements EscapeGameManager<SquareCoordinate> {
+
+	@Override
+	public boolean move(SquareCoordinate from, SquareCoordinate to) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public EscapePiece getPieceAt(SquareCoordinate coordinate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SquareCoordinate makeCoordinate(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
